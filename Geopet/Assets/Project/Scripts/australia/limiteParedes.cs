fca288ba8fd2cfa1f87dee8f7dc9eb96d1387a1b
Adria Lama <adria.lama@enti.cat>
2025-10-10 23:35:39 +0200

    limites

--- Geopet/Assets/Project/Scripts/australia/limiteParedes.cs
+++ Geopet/Assets/Project/Scripts/australia/limiteParedes.cs
@@ -2,18 +2,39 @@ using UnityEngine;
 
 public class limiteParedes : MonoBehaviour
 {
-    public float rightLimit;
-    public float leftLimit;
-    
+    private Camera mainCam;
 
     void Start()
     {
-        
+        mainCam = Camera.main;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        CheckHorizontalWrap();
+    }
+
+    private void CheckHorizontalWrap()
+    {
+
+        Vector3 camPos = mainCam.transform.position;
+        float camHeight = 2f * mainCam.orthographicSize;
+        float camWidth = camHeight * mainCam.aspect;
+        float leftLimit = camPos.x - camWidth / 2f;
+        float rightLimit = camPos.x + camWidth / 2f;
+        Vector3 pos = transform.position;
+
+
+        if (pos.x > rightLimit)
+        {
+            pos.x = leftLimit;
+            transform.position = pos;
+        }
+        else if (pos.x < leftLimit)
+        {
+            pos.x = rightLimit;
+            transform.position = pos;
+        }
     }
 }
