df9859599bbf848a09c3bb6d87992b087b61c7e1
Adria Lama <adria.lama@enti.cat>
2025-10-11 00:03:52 +0200

    mov plataformas

---
+++ Geopet/Assets/Project/Scripts/australia/movPlataformas.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+
+public class movPlataformas : MonoBehaviour
+{
+    public int speed;
+    public float timer;
+    public bool isRight;
+    public bool isLeft;
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(cdMovimiento());
+    }
+
+    IEnumerator cdMovimiento()
+    {
+        if (isRight)
+        {
+            transform.Translate(Vector2.right * Time.deltaTime * speed );
+            yield return new WaitForSeconds(timer);
+            isLeft = true;
+            isRight = false;
+        }
+        else if (isLeft)
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * speed);
+            yield return new WaitForSeconds(timer);
+            isLeft = false;
+            isRight = true;
+        }
+    }
+}
