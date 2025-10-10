b917a81cebba288de02aba55b06b2300eabd1074
NachoErmisch <nachoermisch@gmail.com>
2025-10-10 23:07:56 +0200

    arreglo mecanica australia

--- Geopet/Assets/Project/Scripts/australia/saltoCubo.cs
+++ Geopet/Assets/Project/Scripts/australia/saltoCubo.cs
@@ -2,41 +2,31 @@ using UnityEngine;
 
 public class saltoCubo : MonoBehaviour
 {
-    public int speed;
-    public float horizontal;
+    public float speed = 5f;
+    public float jumpForce = 10f;
     private Rigidbody2D rb2D;
-    public int jumpForce;
-    private bool isGrounded;
-    private bool wasGrounded;
 
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
     }
 
-    
     void FixedUpdate()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-
-        if (horizontal >= 0)
-        {
-            rb2D.linearVelocity = new Vector2(horizontal * speed, rb2D.linearVelocityY);
-            float velocidadHorizontal = Mathf.Abs(rb2D.linearVelocityX);
-        }
-        if (horizontal <= 0)
-        {
-            rb2D.linearVelocity = new Vector2(horizontal * speed, rb2D.linearVelocityY);
-            float velocidadHorizontal = Mathf.Abs(rb2D.linearVelocityX);
-        }
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        rb2D.linearVelocity = new Vector2(horizontal * speed, rb2D.linearVelocityY);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Plataforma"))
-        {
-            rb2D.AddForce(Vector2.up * jumpForce ,ForceMode2D.Impulse);
-            
+        {       
+            ContactPoint2D contact = collision.contacts[0];
+          
+            if (Vector2.Dot(contact.normal, Vector2.up) > 0.5f)
+            {
+                rb2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            }
         }
     }
 }
