19c32b3c95954b3d21a3b173e9aca0ce4d4ce7a6
Adria Lama <adria.lama@enti.cat>
2025-10-15 16:25:43 +0200

    arreglo japon

--- Geopet/Assets/Project/Scripts/japan/movPJ.cs
+++ Geopet/Assets/Project/Scripts/japan/movPJ.cs
@@ -2,55 +2,36 @@ using UnityEngine;
 
 public class movPJ : MonoBehaviour
 {
-    public float speed;
+    public float speed = 5f;
     private Rigidbody2D rb2D;
     private SpriteRenderer sr;
     public Transform cesta;
-    public bool posCesta = false;
+    public float offsetX;
+
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
-        
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         rb2D.linearVelocity = new Vector2(horizontal * speed, rb2D.linearVelocityY);
 
-        if(horizontal >= 1)
+        if (horizontal > 0)
         {
             sr.flipX = true;
-
-            if (posCesta == false)
-            {
-                Vector2 pos = cesta.position;
-                pos.x += 8;
-                cesta.position = pos;
-                posCesta = true;
-            }
-           
-            
+            Vector3 pos = cesta.localPosition;
+            pos.x = offsetX;
+            cesta.localPosition = pos;
         }
-        else if(horizontal <= -1)
+        else if (horizontal < 0)
         {
-            
             sr.flipX = false;
-            
-            if(posCesta == true)
-            {
-                Vector2 pos = cesta.position;
-                pos.x -= 8;
-                cesta.position = pos;
-                posCesta = false;
-            }
-           
+            Vector3 pos = cesta.localPosition;
+            pos.x = -offsetX;
+            cesta.localPosition = pos;
         }
-
-        
-       
     }
-    
 }
