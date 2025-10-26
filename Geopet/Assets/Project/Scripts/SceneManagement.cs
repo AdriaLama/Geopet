563ed85116d42863aa11c673e3659dd94040d6a7
NachoErmisch <nachoermisch@gmail.com>
2025-10-26 23:16:08 +0100

    banderas corregido

--- Geopet/Assets/Project/Scripts/SceneManagement.cs
+++ Geopet/Assets/Project/Scripts/SceneManagement.cs
@@ -1,22 +1,36 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneManagement : MonoBehaviour
 {
+    
     public bool canSpain = true;
     public bool canAustralia = true;
     public bool canJapan = true;
     public bool canArgentina = true;
+    public GameObject buttonEspaña;
+    public GameObject buttonJapon;
+    public GameObject buttonArgentina;
+    public GameObject buttonItalia;
+    public GameObject buttonAustralia;
+
+    public Sprite spriteEspañaCompletado;
+    public Sprite spriteJaponCompletado;
+    public Sprite spriteArgentinaCompletado;
+    public Sprite spriteItaliaCompletado;
+    public Sprite spriteAustraliaCompletado;
+    public Color color = Color.white;
 
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        changeFlags();
     }
 
 
@@ -63,5 +77,62 @@ public class SceneManagement : MonoBehaviour
         }
 
 
+    }
+    public void changeFlags()
+    {
+        if (PlayerPrefs.GetInt("ahorcadoEspaña") == 1)
+        {
+            buttonEspaña.GetComponent<Image>().sprite = spriteEspañaCompletado;
+            buttonEspaña.GetComponent<Image>().color = color;
+
+            Button btn = buttonEspaña.GetComponent<Button>();
+            ColorBlock cb = btn.colors;
+            cb.normalColor = color;
+            btn.colors = cb;
+        }
+
+        if (PlayerPrefs.GetInt("ahorcadoArgentina") == 1)
+        {
+            buttonArgentina.GetComponent<Image>().sprite = spriteArgentinaCompletado;
+            buttonArgentina.GetComponent<Image>().color = color;
+
+            Button btn = buttonArgentina.GetComponent<Button>();
+            ColorBlock cb = btn.colors;
+            cb.normalColor = color;
+            btn.colors = cb;
+        }
+
+        if (PlayerPrefs.GetInt("ahorcadoItalia") == 1)
+        {
+            buttonItalia.GetComponent<Image>().sprite = spriteItaliaCompletado;
+            buttonItalia.GetComponent<Image>().color = color;
+
+            Button btn = buttonItalia.GetComponent<Button>();
+            ColorBlock cb = btn.colors;
+            cb.normalColor = color;
+            btn.colors = cb;
+        }
+
+        if (PlayerPrefs.GetInt("ahorcadoAustralia") == 1)
+        {
+            buttonAustralia.GetComponent<Image>().sprite = spriteAustraliaCompletado;
+            buttonAustralia.GetComponent<Image>().color = color;
+
+            Button btn = buttonAustralia.GetComponent<Button>();
+            ColorBlock cb = btn.colors;
+            cb.normalColor = color;
+            btn.colors = cb;
+        }
+
+        if (PlayerPrefs.GetInt("ahorcadoJapon") == 1)
+        {
+            buttonJapon.GetComponent<Image>().sprite = spriteJaponCompletado;
+            buttonJapon.GetComponent<Image>().color = color;
+
+            Button btn = buttonJapon.GetComponent<Button>();
+            ColorBlock cb = btn.colors;
+            cb.normalColor = color;
+            btn.colors = cb;
+        }
     }
 }
--- Geopet/Assets/Project/Scripts/gameManager.cs
+++ Geopet/Assets/Project/Scripts/gameManager.cs
@@ -5,18 +5,7 @@ using UnityEngine.UI;
 
 public class gameManager : MonoBehaviour
 {
-    public GameObject buttonEspaña;
-    public GameObject buttonJapon;
-    public GameObject buttonArgentina;
-    public GameObject buttonItalia;
-    public GameObject buttonAustralia;
-
-    public Sprite spriteEspañaCompletado;
-    public Sprite spriteJaponCompletado;
-    public Sprite spriteArgentinaCompletado;
-    public Sprite spriteItaliaCompletado;
-    public Sprite spriteAustraliaCompletado;
-    public Color color = Color.white;
+    
 
     public static gameManager Instance;
 
@@ -39,8 +28,8 @@ public class gameManager : MonoBehaviour
 
     void Update()
     {
+       
 
-        changeFlags();
     }
 
     public void AddCoins(int amount)
@@ -58,68 +47,12 @@ public class gameManager : MonoBehaviour
     {
         totalCoins = PlayerPrefs.GetInt("TotalCoins", 0);
 
-        
+       
     }
 
 
 
-    public void changeFlags()
-    {
-        if (PlayerPrefs.GetInt("ahorcadoEspaña") == 1)
-        {
-            buttonEspaña.GetComponent<Image>().sprite = spriteEspañaCompletado;
-            buttonEspaña.GetComponent<Image>().color = color;
-
-            Button btn = buttonEspaña.GetComponent<Button>();
-            ColorBlock cb = btn.colors; 
-            cb.normalColor = color;     
-            btn.colors = cb;            
-        }
-
-        if (PlayerPrefs.GetInt("ahorcadoArgentina") == 1)
-        {
-            buttonArgentina.GetComponent<Image>().sprite = spriteArgentinaCompletado;
-            buttonArgentina.GetComponent<Image>().color = color;
-
-            Button btn = buttonArgentina.GetComponent<Button>();
-            ColorBlock cb = btn.colors;
-            cb.normalColor = color;
-            btn.colors = cb;
-        }
-
-        if (PlayerPrefs.GetInt("ahorcadoItalia") == 1)
-        {
-            buttonItalia.GetComponent<Image>().sprite = spriteItaliaCompletado;
-            buttonItalia.GetComponent<Image>().color = color;
-
-            Button btn = buttonItalia.GetComponent<Button>();
-            ColorBlock cb = btn.colors;
-            cb.normalColor = color;
-            btn.colors = cb;
-        }
-
-        if (PlayerPrefs.GetInt("ahorcadoAustralia") == 1)
-        {
-            buttonAustralia.GetComponent<Image>().sprite = spriteAustraliaCompletado;
-            buttonAustralia.GetComponent<Image>().color = color;
-
-            Button btn = buttonAustralia.GetComponent<Button>();
-            ColorBlock cb = btn.colors;
-            cb.normalColor = color;
-            btn.colors = cb;
-        }
-
-        if (PlayerPrefs.GetInt("ahorcadoJapon") == 1)
-        {
-            buttonJapon.GetComponent<Image>().sprite = spriteJaponCompletado;
-            buttonJapon.GetComponent<Image>().color = color;
-
-            Button btn = buttonJapon.GetComponent<Button>();
-            ColorBlock cb = btn.colors;
-            cb.normalColor = color;
-            btn.colors = cb;
-        }
-    }
+   
 
 
 }
